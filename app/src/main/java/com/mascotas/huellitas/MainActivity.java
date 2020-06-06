e61ed1a8001172c930a02de21dffac3bc8a978c7
jorge paredes <jalbert400@gmail.com>
2020-06-06 13:59:19 -0500

    Se ha subido el primer push de la aplicacion que contiene el icon y el spashScreen basico

---
+++ app/src/main/java/com/mascotas/huellitas/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mascotas.huellitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
