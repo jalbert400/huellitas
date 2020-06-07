fba52432bfc9a22d580e6910e623b42d54856332
jorge paredes <jalbert400@gmail.com>
2020-06-07 03:59:10 -0500

    Se ha subido el splash screen de la forma correcta mejorada

--- app/src/main/java/com/mascotas/huellitas/MainActivity.java
+++ app/src/main/java/com/mascotas/huellitas/MainActivity.java
@@ -9,13 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        setTheme(R.style.AppTheme);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
