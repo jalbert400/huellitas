5491a4cd2ba0c72c0dadbd4b79e6f14b23031bc4
jorge paredes <jalbert400@gmail.com>
2020-06-08 22:45:00 -0500

    Se ha subido el logeo de usuario con storage de sesion

--- app/src/main/java/com/mascotas/huellitas/ListCategoriesActivity.java
+++ app/src/main/java/com/mascotas/huellitas/ListCategoriesActivity.java
@@ -2,13 +2,33 @@ package com.mascotas.huellitas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListCategoriesActivity extends AppCompatActivity {
+
+    Button buttonClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_categories);
+
+        buttonClose = findViewById(R.id.buttonClose);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+                preferences.edit().clear().commit();
+
+                Intent intent = new Intent(ListCategoriesActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
