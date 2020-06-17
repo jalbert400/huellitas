5ec5925979faad392e5c97304efbcd3519ad067f
jorge paredes <jalbert400@gmail.com>
2020-06-17 01:18:21 -0500

    Se a agregado mejoras al listado de categorias, y esta listo para pasar a subcategorias

---
+++ app/src/main/java/com/mascotas/huellitas/Fragments/MainFragment.java
@@ -0,0 +1,22 @@
+package com.mascotas.huellitas.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.mascotas.huellitas.R;
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container,false);
+        return view;
+    }
+}
